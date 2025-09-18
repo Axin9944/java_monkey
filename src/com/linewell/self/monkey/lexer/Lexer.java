df96f0ab8a1daaf25fc31305e18fe3d74c14886e
Axin <axinnoemail@gmail.com>
2025-09-18 20:02:29 +0800

    增加识别关键字功能

--- src/com/linewell/self/monkey/lexer/Lexer.java
+++ src/com/linewell/self/monkey/lexer/Lexer.java
@@ -31,6 +31,8 @@ public class Lexer {
     public Token nextToken() {
         Token tok;
 
+        skipWhitespace();
+
         switch (ch) {
             case '=':
                 tok = newToken(TokenType.ASSIGN, ch);
@@ -60,9 +62,13 @@ public class Lexer {
                 tok = new Token(TokenType.EOF, "");
                 break;
             default:
-                // 目前没有处理非法字符，可以扩展
-                tok = new Token(TokenType.ILLEGAL, String.valueOf(ch));
-                break;
+                if (isLetter(ch)) {
+                    String literal = readIdentifier();
+                    tok = newToken(TokenType.loopupIdent(literal),
+                            literal);
+                } else {
+                    tok = newToken(TokenType.ILLEGAL, ch);
+                }
         }
 
         readChar();  // 移动到下一个字符
@@ -73,4 +79,47 @@ public class Lexer {
         return new Token(tokenType, String.valueOf(ch));
     }
 
+    private Token newToken(TokenType tokenType, String ch) {
+        return new Token(tokenType, ch);
+    }
+
+    /***
+     *  判断字符是否为字母
+     * @param ch
+     * @return
+     */
+    public boolean isLetter(char ch) {
+        return  ('a' <= ch && ch <= 'z') ||
+                ('A' <= ch && ch <= 'Z') ||
+                (ch == '_');
+    }
+
+    /***
+     *  读取标识符（连续的字母、数字、下划线）
+     *  从当前位置开始，直到遇到非字母字符为止
+     * @return 标识符字符串
+     */
+    public String readIdentifier() {
+        // 记录起始为止
+        int start = position;
+
+        while (isLetter(ch)) {
+            // 不断前移，直至遇见非字母
+            readChar();
+        }
+        // 截取 input 中 [start, position) 的子字符串
+        return input.substring(start, position);
+    }
+
+    /***
+     *  跳过空白字符
+     */
+    public void skipWhitespace() {
+        while ((ch == ' ')
+                || (ch == '\t')
+                || (ch == '\n')
+                || (ch == '\r')) {
+            readChar();
+        }
+    }
 }
--- src/com/linewell/self/monkey/token/TokenType.java
+++ src/com/linewell/self/monkey/token/TokenType.java
@@ -1,5 +1,8 @@
 package com.linewell.self.monkey.token;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TokenType {
     ILLEGAL("ILLEGAL"),
     EOF("EOF"),
@@ -36,5 +39,23 @@ public enum TokenType {
     @Override
     public String toString() {
         return literal;
+    }
+
+    // 关键字映射表
+    private static final Map<String, TokenType> keywords = new HashMap<>();
+
+    static {
+        keywords.put("fn", FUNCTION);
+        keywords.put("let", LET);
+    }
+
+    /***
+     * 查找标识符是否为关键字
+     * @param ident 标识符名称
+     * @return  对应的 TokenType, 如果不是关键字，返回 Token.IDENT
+     */
+    public static TokenType loopupIdent(String ident) {
+        TokenType tokenType = keywords.get(ident);
+        return tokenType != null ? tokenType : IDENT;
     }
 }
